70da1c32be6b7bca667d27d6f0d3b56c1652485c
Sreepathi Pai <sree@buckyball>
2021-01-11 11:28:11 -0500

    Fix naming (EPBF->EBPF) and use JGT to fix rename bug in ssa

--- ebpf.py
+++ ebpf.py
@@ -128,7 +128,7 @@ def EBPF_OP_ARSH64(value: s64, shift: u64) -> s64:
 def EBPF_OP_JA(pc: u16, offset: u16) -> u16:
     return pc + offset
 
-def EPBF_OP_JEQ(pc: u16, offset: u16, a: u64, b: u64) -> u16:
+def EBPF_OP_JEQ(pc: u16, offset: u16, a: u64, b: u64) -> u16:
     # is b sign-extended to 64-bits when passed as an immediate (JEQ_IMM) in vm?
     if a == b:
         dst = pc + offset
@@ -137,17 +137,14 @@ def EPBF_OP_JEQ(pc: u16, offset: u16, a: u64, b: u64) -> u16:
 
     return dst
 
-def EPBF_OP_JGT(pc: u16, offset: u16, a: u64, b: u64) -> u16:
+def EBPF_OP_JGT(pc: u16, offset: u16, a: u64, b: u64) -> u16:
     # b should not be sign-extended to 64-bits when passed as an immediate (JGT_IMM)
-    # TODO: need to be able to write this as pc += offset, waiting for params bugfix for smt2 backend
     if a > b:
-        dst = pc + offset
-    else:
-        dst = pc
+        pc += offset
 
     return pc
 
-def EPBF_OP_JGE(pc: u16, offset: u16, a: u64, b: u64) -> u16:
+def EBPF_OP_JGE(pc: u16, offset: u16, a: u64, b: u64) -> u16:
     # b should not be sign-extended to 64-bits when passed as an immediate (JGE_IMM)
     if a >= b:
         dst = pc + offset
@@ -156,7 +153,7 @@ def EPBF_OP_JGE(pc: u16, offset: u16, a: u64, b: u64) -> u16:
 
     return dst
 
-def EPBF_OP_JSET(pc: u16, offset: u16, a: u64, b: u64) -> u16:
+def EBPF_OP_JSET(pc: u16, offset: u16, a: u64, b: u64) -> u16:
     # sign-extension of b unclear
     if (a & b) != 0:
         dst = pc + offset
@@ -165,7 +162,7 @@ def EPBF_OP_JSET(pc: u16, offset: u16, a: u64, b: u64) -> u16:
 
     return dst
 
-def EPBF_OP_JNE(pc: u16, offset: u16, a: u64, b: u64) -> u16:
+def EBPF_OP_JNE(pc: u16, offset: u16, a: u64, b: u64) -> u16:
     # sign extension of b unclear in spec
     if a != b:
         dst = pc + offset
@@ -174,7 +171,7 @@ def EPBF_OP_JNE(pc: u16, offset: u16, a: u64, b: u64) -> u16:
 
     return dst
 
-def EPBF_OP_JSGT(pc: u16, offset: u16, a: s64, b: s64) -> u16:
+def EBPF_OP_JSGT(pc: u16, offset: u16, a: s64, b: s64) -> u16:
     if a > b:
         dst = pc + offset
     else:
@@ -182,7 +179,7 @@ def EPBF_OP_JSGT(pc: u16, offset: u16, a: s64, b: s64) -> u16:
 
     return dst
 
-def EPBF_OP_JSGE(pc: u16, offset: u16, a: s64, b: s64) -> u16:
+def EBPF_OP_JSGE(pc: u16, offset: u16, a: s64, b: s64) -> u16:
     if a >= b:
         dst = pc + offset
     else:
@@ -193,7 +190,7 @@ def EPBF_OP_JSGE(pc: u16, offset: u16, a: s64, b: s64) -> u16:
 # call
 # exit
 
-def EPBF_OP_JLT(pc: u16, offset: u16, a: u64, b: u64) -> u16:
+def EBPF_OP_JLT(pc: u16, offset: u16, a: u64, b: u64) -> u16:
     # b should not be sign-extended to 64-bits when passed as an immediate (JLT_IMM)
     if a < b:
         dst = pc + offset
@@ -203,7 +200,7 @@ def EPBF_OP_JLT(pc: u16, offset: u16, a: u64, b: u64) -> u16:
     return dst
 
 
-def EPBF_OP_JLE(pc: u16, offset: u16, a: u64, b: u64) -> u16:
+def EBPF_OP_JLE(pc: u16, offset: u16, a: u64, b: u64) -> u16:
     # b should not be sign-extended to 64-bits when passed as an immediate (JLE_IMM)
     if a <= b:
         dst = pc + offset
@@ -213,7 +210,7 @@ def EPBF_OP_JLE(pc: u16, offset: u16, a: u64, b: u64) -> u16:
     return dst
 
 
-def EPBF_OP_JSLT(pc: u16, offset: u16, a: s64, b: s64) -> u16:
+def EBPF_OP_JSLT(pc: u16, offset: u16, a: s64, b: s64) -> u16:
     if a < b:
         dst = pc + offset
     else:
@@ -221,7 +218,7 @@ def EPBF_OP_JSLT(pc: u16, offset: u16, a: s64, b: s64) -> u16:
 
     return dst
 
-def EPBF_OP_JSLE(pc: u16, offset: u16, a: s64, b: s64) -> u16:
+def EBPF_OP_JSLE(pc: u16, offset: u16, a: s64, b: s64) -> u16:
     if a <= b:
         dst = pc + offset
     else:
